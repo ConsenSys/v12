35744c1f1e73a86220a3b5a612cd98266d94226a
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2018-06-19 11:42:46 +1000

    Merge branch 'update-version'

--- scripts/start_ganache.sh
+++ scripts/start_ganache.sh
@@ -1,1 +1,1 @@
-./ganache-cli -m "whip venture public clip similar debris minimum mandate despair govern rotate swim"
+../node_modules/.bin/ganache-cli -p 8545 -m "whip venture public clip similar debris minimum mandate despair govern rotate swim"
