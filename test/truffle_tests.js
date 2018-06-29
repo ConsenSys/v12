650899b275ca2cdee16b556b9fda2f0d044fe1c1
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2018-06-29 10:00:47 +1000

    Try use truffle

---
+++ test/truffle_tests.js
@@ -0,0 +1,19 @@
+const Exchange = artifacts.require('DepositProxy');
+//const assertJump = require("./assertJump.js");
+
+const durationTime = 28; //4 weeks
+
+contract('Deposit', function(accounts) {
+  beforeEach(async function () {
+    //const alice = '0xa5f8ff129c19dbc0849619916c16010738ab5b1f';
+    this.exchange = await Exchange.new(); //accounts[0]
+    //this.exchange = await Exchange.new();
+  });
+
+  describe("Test new exchange features", async function() {
+    it("should have ...", async function () {
+      //const actual = await this.exchange.INACTIVITY_CAP();
+      //assert.equal(actual, '0x00', "Cap should be ");
+    });
+  });
+});
