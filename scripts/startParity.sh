9ac5d936b13b1f592401848145fc6fd7b9f3bb84
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2018-07-26 14:02:24 +1000

    Add key file and remove parity

--- scripts/startParity.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-parity --chain testchainSpec.json --config parityDevConfig.toml --ws-port 8647
-
-# generates a UI token
-#parity --chain testchainSpec.json --config parityDevConfig.toml signer new-token
