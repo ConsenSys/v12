592155010885a6670f28c638f61df999c6190852
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2018-08-02 10:56:58 +1000

    Auto stash before merge of "master" and "origin/master"

--- truffle.js
+++ truffle.js
@@ -1,15 +1,15 @@
 module.exports = {
   networks: {
-    development: {
+    unspecified: {
       host: "localhost",
       port: 8545,
       network_id: "*", // Match any network id
-      gas: 6721975
+      gas: 4600000
     },
     rinkeby:  {
     network_id: 4,
     host: "localhost",
     port: 8545,
-    gas: 2900000
+    gas: 4600000
   }}
 };
